954e399a8dcb95144b1c12f22fd154e62dd0c614
raymond <raymond.korir@student.moringaschool.com>
2024-04-11 20:38:24 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+  }
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
